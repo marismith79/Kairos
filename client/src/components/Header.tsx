1d372442a0abf6ca2d867d96534fac88659217aa
Shomari Smith <mari@Shomaris-MacBook-Air-2.local>
2025-01-22 20:28:56 -0500

    Fixed header and other formatting

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,28 +1,32 @@
 import "../App.css";
-
 import { Button } from "../components/ui/button";
 import { Link } from "wouter";
 
 export function Header() {
   return (
-    <header>
-      <nav>
-        <Link href="/about">
-          <Button>
-            About
-          </Button>
-        </Link>
-        <Link href="/chat">
-        <Button>
-            Chat
-        </Button>
-        </Link>
-        <Link href="/home">
-        <Button>
-            Home
-        </Button>
-        </Link>
-      </nav>
+    <header className="header-container" >
+      <div className="Header1">
+        <p>Kairos</p>
+      </div>
+      <div className="Header2">
+        <nav className="nav">
+          <Link href="/about">
+            <Button>
+              About
+            </Button>
+          </Link>
+          <Link href="/chat">
+            <Button>
+              Chat
+            </Button>
+          </Link>
+          <Link href="/home">
+            <Button>
+              Home
+            </Button>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
